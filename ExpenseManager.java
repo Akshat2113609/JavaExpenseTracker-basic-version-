e76577451c89a4d240c0c995bc58290aed301451
Akshat <akshatawasthi21@gmail.com>
2026-02-14 18:22:05 +0530

    modified the expense manager with Hashmap logic to improve scalability

--- ExpenseManager.java
+++ ExpenseManager.java
@@ -1,5 +1,16 @@
 import java.util.*;
 class ExpenseManager {
+    public static HashMap<String,ArrayList<Double>> createMap(ArrayList<Expense>expenses){
+        HashMap<String,ArrayList<Double>> expenseMap = new HashMap<>();
+        for(Expense e: expenses){
+            if(!expenseMap.containsKey(e.category)){
+                expenseMap.put(e.category,new ArrayList<>());
+            }
+            expenseMap.get(e.category).add(e.amount);
+        }
+        return expenseMap;
+    }
+
     static double totalExpenses(ArrayList<Expense>expenses){
         double totalSpending=0.0;
         for(int i=0;i<expenses.size();i++){
@@ -10,41 +21,21 @@ class ExpenseManager {
         return totalSpending;
   } 
   static void totalExpensesWithCategory(ArrayList<Expense>expenses){
-     String cat1="Food";
-     String cat2="Entertainment";
-     String cat3="Travel";
-     String cat4="Leisure";
-     double amount1=0.0;
-     double amount2=0.0;
-     double amount3=0.0;
-     double amount4=0.0;
-
-     for(Expense p : expenses){
-        if(p.category.equals(cat1)){
-            amount1+=p.amount;
+     HashMap<String,ArrayList<Double>> expenseMap = createMap(expenses);
+     for(String cat: expenseMap.keySet()){
+        double amnt = 0.0;
+        for(double i: expenseMap.get(cat)){
+            amnt+=i;
         }
-        else if(p.category.equals(cat2)){
-            amount2+=p.amount;
-        }
-        else if(p.category.equals(cat3)){
-            amount3+=p.amount;
-        }
-        else{
-            amount4+=p.amount;
-        }
+        System.out.println("For category: " + cat + " the amount is: " + amnt);
      }
-     System.out.println("for category: "+ cat1 + " the amount spent is: "+ amount1);
-     System.out.println("for category: "+ cat2 + " the amount spent is: "+ amount2);
-     System.out.println("for category: "+ cat3 + " the amount spent is: "+ amount3);
-     System.out.println("for category: "+ cat4 + " the amount spent is: "+ amount4);
   }
 
   static void categoryExpense(ArrayList<Expense>expenses, String cat){
+    HashMap<String,ArrayList<Double>> expenseMap = createMap(expenses);
     double amnt = 0.0;
-        for(Expense p: expenses){
-            if(p.category.equals(cat)){
-                amnt+=p.amount;
-            }
+        for(double i: expenseMap.get(cat)){
+            amnt+=i;
         }
     System.out.println("for category: "+ cat + "the amount spent is: "+ amnt); 
   }
